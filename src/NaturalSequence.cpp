c2a5bdd9bec5be8780b32f1ccbed01f1b0b200ba
virtustron <virtustron@gmail.com>
2021-10-05 19:42:46 +0300

    Add lower and upper bounds to NaturalSequence

--- src/NaturalSequence.cpp
+++ src/NaturalSequence.cpp
@@ -1,12 +1,20 @@
 #include "NaturalSequence.hpp"
 
+NaturalSequence::NaturalSequence(const unsigned int lower_bound, const unsigned int upper_bound)
+{
+    // TODO add validation
+    m_lower_bound = lower_bound;
+    m_upper_bound = upper_bound;
+}
+
+
 NaturalSequenceIterator NaturalSequence::begin() 
 {
-    return NaturalSequenceIterator(0);
+    return NaturalSequenceIterator(m_lower_bound);
 }
 
 NaturalSequenceIterator NaturalSequence::end() 
 {
-    return NaturalSequenceIterator(UINT_MAX);
+    return NaturalSequenceIterator(m_upper_bound);
 }
 
--- src/NaturalSequence.hpp
+++ src/NaturalSequence.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <limits.h>
 #include "NaturalSequenceIterator.hpp"
 
 
@@ -8,6 +9,12 @@
 class NaturalSequence
 {
 public:
+    NaturalSequence(const unsigned int lower_bound = 0, const unsigned int upper_bound = UINT_MAX);
+    
     NaturalSequenceIterator begin();
     NaturalSequenceIterator end();
+
+private:
+    unsigned int m_lower_bound;
+    unsigned int m_upper_bound;
 };
